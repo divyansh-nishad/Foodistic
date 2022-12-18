9b3993ebba37ddb4045f54feddd633438c37b3f4
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-18 20:10:49 +0530

    -foodistic update

---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:foodistic/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "4598879b43mshbaa541ef9645388p1886d3jsn7b9fb3bd8dcb",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    print(data);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
